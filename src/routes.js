d5790cd90c2c7551db74f4dde8f5ff7ce351672c
Admin <rtektano@gmail.com>
2023-10-03 17:30:30 +0700

    Add middleware

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,34 @@
-import { useRoutes } from 'react-router-dom';
+import { redirect, useRoutes } from 'react-router-dom';
 import FormVendorPage from './pages/FormVendorPage';
+import axios from 'axios';
+
+async function formLoader({ formType, token }) {
+  if (formType == 'newform') {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_URL_LOC}/form/new/${token}`);
+      if (response.status == 401) {
+        redirect('/login');
+      } else {
+        return response.data;
+      }
+    } catch (error) {
+      redirect('/404');
+      return 0;
+    }
+  } else if (formType == 'form') {
+    return { data: 'yes' };
+  }
+}
 
 export default function Router() {
-  const routes = useRoutes([{ path: '/:formtype/:token', element: <FormVendorPage /> }]);
+  const routes = useRoutes([
+    {
+      path: 'frm/:formtype/:token',
+      loader: (params) => {
+        formLoader(params);
+      },
+      element: <FormVendorPage />,
+    },
+  ]);
   return routes;
 }
