9a0e215677e0ddb195764b3b4a8ec51393ff98e6
tectanagrand <rtektano@gmail.com>
2023-10-20 16:37:50 +0700

    Filename Change : authProvider => sessionProvider

---
+++ src/provider/sessionProvider.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useContext, createContext, useState, useEffect, useMemo } from 'react';
+import Cookies from 'js-cookie';
+
+const SessionContext = createContext();
+
+const SessionProvider = ({ children }) => {
+  const [session, setSession_] = useState({
+    fullname: Cookies.get('fullname'),
+    username: Cookies.get('username'),
+    email: Cookies.get('email'),
+    jwttoken: Cookies.get('jwttoken'),
+    user_id: Cookies.get('user_id'),
+    role: Cookies.get('role'),
+  });
+  const setSession = ({ fullname, username, email, token, id, role }) => {
+    setSession_({ fullname: fullname, username: username, email: email, jwttoken: token, user_id: id, role: role });
+  };
+  useEffect(() => {
+    if (session.jwttoken) {
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + session.jwttoken;
+      Cookies.set('jwttoken', session.jwttoken);
+      Cookies.set('fullname', session.fullname);
+      Cookies.set('email', session.email);
+      Cookies.set('username', session.username);
+      Cookies.set('user_id', session.user_id);
+      Cookies.set('role', session.role);
+    } else {
+      Cookies.remove('jwttoken');
+      Cookies.remove('fullname');
+      Cookies.remove('email');
+      Cookies.remove('username');
+      Cookies.remove('user_id');
+      Cookies.remove('role');
+    }
+  }, [session]);
+
+  const contextValue = useMemo(() => ({ session, setSession }), [session]);
+
+  return <SessionContext.Provider value={contextValue}>{children}</SessionContext.Provider>;
+};
+
+export const useSession = () => {
+  return useContext(SessionContext);
+};
+
+export default SessionProvider;
