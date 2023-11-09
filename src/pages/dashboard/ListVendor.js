f532802cab80ddd5e07d14648eae621e290c15d6
tectanagrand <rtektano@gmail.com>
2023-11-09 16:59:37 +0700

    Feature : Add function create request activate

--- src/pages/dashboard/ListVendor.js
+++ src/pages/dashboard/ListVendor.js
@@ -9,6 +9,8 @@ import {
   DialogTitle,
   DialogActions,
   TextField,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { useEffect, useRef, useState } from 'react';
@@ -26,18 +28,28 @@ export default function ListVendor() {
   };
 
   const [filterAct, setFilteract] = useState('');
+  const [formStat, setFormstat] = useState({
+    stat: false,
+    type: '',
+    message: '',
+  });
   const [dialogOpen, setDialog] = useState(false);
   const [btnState, setBtnst] = useState({});
   const [data, setData] = useState([]);
   const [request, setRequest] = useState({
+    ven_id: '',
     ven_code: '',
     ven_name: '',
     reason: '',
     requestor: '',
+    requestor_id: '',
   });
-  // const handleChange = (e) => {
-  //   setFilteract(e.)
-  // }
+  const handleSnackClose = (e, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setFormstat({ ...formStat, stat: false });
+  };
 
   const getData = async () => {
     const data = await axios.get(`${process.env.REACT_APP_URL_LOC}/vendor/?isactive=${filterAct}`);
@@ -52,11 +64,46 @@ export default function ListVendor() {
       setBtnst({ color: 'success', text: 'Reactivate' });
     }
     setRequest({
+      ven_id: row.id,
       ven_code: row.ven_code,
       ven_name: row.ven_name,
       reason: row.act_remark,
       requestor: session.email,
+      requestor_id: session.user_id,
+      is_active: row.is_active,
     });
+  };
+
+  const submitReq = async () => {
+    try {
+      const requestType = request.is_active === true ? 0 : 1;
+      const response = await axios.post(`${process.env.REACT_APP_URL_LOC}/reqstat/new`, {
+        ven_id: request.ven_id,
+        remarks: request.reason,
+        request: requestType,
+        requestor: request.requestor_id,
+      });
+      const message = response.data.message;
+      setFormstat({
+        stat: true,
+        type: 'success',
+        message: message,
+      });
+    } catch (error) {
+      setFormstat({
+        stat: true,
+        type: 'error',
+        message: 'error',
+      });
+      console.error(error);
+    }
+  };
+
+  const handleReason = (e) => {
+    setRequest((prevRequest) => ({
+      ...prevRequest,
+      reason: e.target.value,
+    }));
   };
 
   const handleCloseMdl = () => {
@@ -70,7 +117,6 @@ export default function ListVendor() {
   useEffect(() => {
     const response = async () => {
       const items = await getData();
-      console.log(items);
       setData(items.data);
     };
     response();
@@ -153,18 +199,29 @@ export default function ListVendor() {
         <Box sx={{ width: 800, height: '100%', padding: 3, display: 'flex', flexDirection: 'column', gap: 2 }}>
           <TextField id="ven_code" label="Vendor Code" value={request.ven_code} inputProps={{ readOnly: true }} />
           <TextField id="name_1" label="Name" value={request.ven_name} inputProps={{ readOnly: true }} />
-          <TextField id="reason" label="Reason" value={request.reason} multiline />
+          <TextField id="reason" label="Reason" value={request.reason} onChange={handleReason} multiline />
           <TextField id="request" label="Request By" value={request.requestor} inputProps={{ readOnly: true }} />
         </Box>
         <DialogActions>
           <Button sx={{ width: 120, m: 1 }} color="secondary" onClick={handleCloseMdl}>
             <Typography>Cancel</Typography>
           </Button>
-          <Button sx={{ width: 120, m: 1 }} variant="contained" color={btnState.color}>
+          <Button sx={{ width: 120, m: 1 }} variant="contained" color={btnState.color} onClick={submitReq}>
             <Typography>{btnState.text}</Typography>
           </Button>
         </DialogActions>
       </Dialog>
+
+      <Snackbar
+        open={formStat.stat}
+        onClose={handleSnackClose}
+        autoHideDuration={3000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Alert severity={formStat.type} onClose={handleSnackClose} variant="filled">
+          {formStat.message}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
