a0db6dd0513bc25b3755f0c1eea22e6076f8f310
tectanagrand <rtektano@gmail.com>
2023-11-20 00:04:38 +0700

    refactor : refactor select component

--- src/components/common/SelectComp.js
+++ src/components/common/SelectComp.js
@@ -1,7 +1,8 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { Controller } from 'react-hook-form';
+import { useState, useEffect } from 'react';
 
-export default function SelectComp({ name, control, label, options, rules }) {
+export default function SelectComp({ name, control, label, options, onChangeovr, rules, disabled, valueovr }) {
   const generateSingleOptions = () => {
     return options.map((item) => {
       return (
@@ -12,16 +13,36 @@ export default function SelectComp({ name, control, label, options, rules }) {
     });
   };
   return (
-    <FormControl fullWidth sx={{ mt: 2, mb: 2 }}>
-      <InputLabel>{label}</InputLabel>
+    <FormControl fullWidth sx={{ mt: 2, mb: 2 }} disabled={disabled}>
       <Controller
-        render={({ field: { onChange, value } }) => (
-          <Select onChange={onChange} value={value} fullWidth label={label}>
-            {generateSingleOptions()}
-          </Select>
-        )}
+        render={({ field: { onChange, value }, fieldState: { error } }) => {
+          if (disabled) {
+            value = '';
+          }
+          return (
+            <>
+              <InputLabel error={!!error}>{label}</InputLabel>
+              <Select
+                fullWidth
+                error={!!error}
+                label={label}
+                value={value}
+                onChange={(e) => {
+                  onChange(e);
+                  if (onChangeovr != undefined) {
+                    onChangeovr(e.target.value);
+                  }
+                }}
+              >
+                {generateSingleOptions()}
+              </Select>
+            </>
+          );
+        }}
         control={control}
         name={name}
+        rules={rules}
+        defaultValue={valueovr}
       />
     </FormControl>
   );
