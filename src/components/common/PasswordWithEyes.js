3337380680b4d45e728ec845640b9931b2c9f982
tectanagrand <rtektano@gmail.com>
2023-11-20 00:04:03 +0700

    refactor : password with eyes

---
+++ src/components/common/PasswordWithEyes.js
@@ -0,0 +1,48 @@
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton } from '@mui/material';
+import { Controller } from 'react-hook-form';
+import { useState } from 'react';
+
+export const PasswordWithEyes = ({ control, label, name, rules }) => {
+  const [showPassword, setPwd] = useState(false);
+
+  const handleClickShowPassword = () => setPwd((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
+          <FormControl fullWidth variant="outlined" sx={{ mt: 2 }}>
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              onChange={onChange}
+              value={value}
+              error={error}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label={label}
+            />
+          </FormControl>
+        )}
+      />
+    </>
+  );
+};
