276c8bba3a4507489f8560e68e6e21c3e3e75b6c
tectanagrand <rtektano@gmail.com>
2023-11-22 17:32:33 +0700

    refactor : session access, refresh, and permission

--- src/provider/sessionProvider.js
+++ src/provider/sessionProvider.js
@@ -9,46 +9,74 @@ const SessionProvider = ({ children }) => {
     fullname: Cookies.get('fullname'),
     username: Cookies.get('username'),
     email: Cookies.get('email'),
-    jwttoken: Cookies.get('jwttoken'),
+    accessToken: Cookies.get('accesstoken'),
+    refreshToken: Cookies.get('refreshtoken'),
     user_id: Cookies.get('user_id'),
     role: Cookies.get('role'),
+    permission: JSON.parse(sessionStorage.getItem('permission')),
+    groupid: Cookies.get('groupid'),
   });
-  const setSession = ({ fullname, username, email, token, id, role, permission }) => {
+  const setSession = (data) => {
     setSession_({
-      fullname: fullname,
-      username: username,
-      email: email,
-      jwttoken: token,
-      user_id: id,
-      role: role,
-      permission: permission,
+      fullname: data.fullname,
+      username: data.username,
+      email: data.email,
+      accessToken: data.accessToken,
+      refreshToken: data.refreshToken,
+      user_id: data.user_id,
+      role: data.role,
+      permission: data.permission,
+      groupid: data.groupid,
     });
   };
   const logOut = () => {
     setSession_({});
     sessionStorage.clear();
   };
-  useEffect(() => {
-    if (session.jwttoken) {
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + session.jwttoken;
-      Cookies.set('jwttoken', session.jwttoken);
-      Cookies.set('fullname', session.fullname);
-      Cookies.set('email', session.email);
-      Cookies.set('username', session.username);
-      Cookies.set('user_id', session.user_id);
-      Cookies.set('role', session.role);
-      sessionStorage.setItem('permission', JSON.stringify(session.permission));
-    } else {
-      Cookies.remove('jwttoken');
-      Cookies.remove('fullname');
-      Cookies.remove('email');
-      Cookies.remove('username');
-      Cookies.remove('user_id');
-      Cookies.remove('role');
+  const getPermission = (page) => {
+    if (sessionStorage.getItem('permission') === null) {
+      return '';
     }
+    const permissions = JSON.parse(sessionStorage.getItem('permission'));
+    const curPermission = permissions[page];
+    return curPermission;
+  };
+
+  useEffect(() => {
+    const addSession = setTimeout(() => {
+      if (session.accessToken) {
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + session.refreshToken;
+        Cookies.set('refreshtoken', session.refreshToken);
+        Cookies.set('accesstoken', session.accessToken);
+        Cookies.set('fullname', session.fullname);
+        Cookies.set('email', session.email);
+        Cookies.set('username', session.username);
+        Cookies.set('user_id', session.user_id);
+        Cookies.set('role', session.role);
+        Cookies.set('groupid', session.groupid);
+        sessionStorage.setItem('permission', JSON.stringify(session.permission));
+      } else {
+        Cookies.remove('fullname');
+        Cookies.remove('email');
+        Cookies.remove('username');
+        Cookies.remove('user_id');
+        Cookies.remove('role');
+        Cookies.remove('accesstoken');
+        Cookies.remove('refreshtoken');
+        Cookies.remove('groupid');
+        sessionStorage.clear();
+      }
+      return () => {
+        clearTimeout(addSession);
+      };
+    }, 500);
+
+    return () => {
+      clearTimeout(addSession);
+    };
   }, [session]);
 
-  const contextValue = useMemo(() => ({ session, setSession, logOut }), [session]);
+  const contextValue = useMemo(() => ({ session, setSession, logOut, getPermission }), [session]);
 
   return <SessionContext.Provider value={contextValue}>{children}</SessionContext.Provider>;
 };
