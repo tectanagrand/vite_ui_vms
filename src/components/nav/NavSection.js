e64cb2899d3c8bd891fc42101df0cc8b24090969
tectanagrand <rtektano@gmail.com>
2023-11-30 08:08:09 +0700

    Refactor : add permission check in nav

--- src/components/nav/NavSection.js
+++ src/components/nav/NavSection.js
@@ -3,50 +3,59 @@ import NavHead from './NavHead';
 import { List } from '@mui/material';
 import Collapse from '@mui/material/Collapse';
 import NavCollapse from './NavCollapse';
+import { useSession } from 'src/provider/sessionProvider';
 
 export default function NavSection({ menu, collapsemen, navmen, onUpNavCol, onUpNavMenu }) {
   const onClickNavHead = (item) => {
     onUpNavCol(item);
   };
-
+  const { getPermission } = useSession();
   return (
     <List>
       {menu.map((item) => {
-        return (
-          <div key={`div-${item.key}`}>
-            <NavHead
-              key={item.key}
-              keyhead={item.key}
-              text={item.text}
-              icon={item.icon}
-              upNav={onClickNavHead}
-              curstate={collapsemen}
-            />
-            {/* <Collapse in={false} timeout="auto" unmountOnExit>
-              <List>
-                {item.children.map((child) => {
-                  return <NavItem key={child.key} keynav={child.key} text={child.text} url={child.url} />;
-                })}
-              </List>
-            </Collapse> */}
-            <NavCollapse parent={item.key} curstate={collapsemen}>
-              <List>
-                {item.children.map((child) => {
-                  return (
-                    <NavItem
-                      key={child.key}
-                      keynav={child.key}
-                      text={child.text}
-                      url={child.url}
-                      upNavMenu={onUpNavMenu}
-                      curstate={navmen}
-                    />
-                  );
-                })}
-              </List>
-            </NavCollapse>
-          </div>
-        );
+        if (getPermission(item.text).read === true) {
+          return (
+            <div key={`div-${item.key}`}>
+              <NavHead
+                key={item.key}
+                keyhead={item.key}
+                text={item.text}
+                icon={item.icon}
+                upNav={onClickNavHead}
+                curstate={collapsemen}
+              />
+              {/* <Collapse in={false} timeout="auto" unmountOnExit>
+                <List>
+                  {item.children.map((child) => {
+                    return <NavItem key={child.key} keynav={child.key} text={child.text} url={child.url} />;
+                  })}
+                </List>
+              </Collapse> */}
+              <NavCollapse parent={item.key} curstate={collapsemen}>
+                <List>
+                  {item.children.map((child) => {
+                    if (getPermission(child.text).read)
+                      return (
+                        <NavItem
+                          key={child.key}
+                          keynav={child.key}
+                          text={child.text}
+                          url={child.url}
+                          upNavMenu={onUpNavMenu}
+                          curstate={navmen}
+                        />
+                      );
+                    else {
+                      return <></>;
+                    }
+                  })}
+                </List>
+              </NavCollapse>
+            </div>
+          );
+        } else {
+          return <></>;
+        }
       })}
     </List>
   );
