20bd0a2350ef090cabab83fe7d66806d8d9258d7
tectanagrand <rtektano@gmail.com>
2023-12-18 09:28:05 +0700

    add: auto complete on bank table

--- src/components/common/AutoCompleteCustom.js
+++ src/components/common/AutoCompleteCustom.js
@@ -1,4 +1,5 @@
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, createFilterOptions } from '@mui/material';
+import ModalCreateBank from './ModalCreateBank';
 import { useGridApiContext } from '@mui/x-data-grid';
 
 export default function AutoCompleteCustom(params) {
@@ -7,18 +8,54 @@ export default function AutoCompleteCustom(params) {
     const { id, row, field } = paramsa;
     apiRef.current.setEditCellValue({ id, field, value: newvalue });
   };
+  const newAddModal = (e) => {
+    params.newAddModal(e.inputValue);
+  };
+  const filter = createFilterOptions();
   return (
-    <Autocomplete
-      name={params.name}
-      options={params.options}
-      value={params.value}
-      onChange={(ev, e) => handleChange(params, e)}
-      onClose={(ev) => {
-        ev.preventDefault();
-      }}
-      isOptionEqualToValue={(option, value) => option.id === value.id}
-      fullWidth
-      renderInput={(paramb) => <TextField {...paramb} label={params.label} fullWidth />}
-    />
+    <>
+      <Autocomplete
+        name={params.name}
+        options={params.options}
+        value={params.value}
+        onChange={(ev, e) => {
+          if (typeof e === 'string') {
+            setTimeout(() => {
+              newAddModal(e);
+            });
+          } else if (e && e.inputValue) {
+            newAddModal(e);
+          } else {
+            handleChange(params, e);
+          }
+        }}
+        onClose={(ev) => {
+          ev.preventDefault();
+        }}
+        filterOptions={(options, param) => {
+          const filtered = filter(options, param);
+          if (param.inputValue !== '' && params.addnew) {
+            filtered.push({
+              inputValue: param.inputValue,
+              label: `Add "${param.inputValue}"`,
+            });
+          }
+          return filtered;
+        }}
+        getOptionLabel={(option) => {
+          if (typeof option === 'string') {
+            return option;
+          }
+          if (option.inputValue) {
+            return option.inputValue;
+          }
+          return option.label;
+        }}
+        renderOption={(props, option) => <li {...props}>{option.label}</li>}
+        isOptionEqualToValue={(option, value) => option.id === value.id}
+        fullWidth
+        renderInput={(paramb) => <TextField {...paramb} label={params.label} fullWidth />}
+      />
+    </>
   );
 }
