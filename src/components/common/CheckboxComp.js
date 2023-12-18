f1eebca16edb950736c90c7bf93267756f3eb10a
tectanagrand <rtektano@gmail.com>
2023-12-18 09:30:15 +0700

    refactor: onchange tracking on checkbox component

--- src/components/common/CheckboxComp.js
+++ src/components/common/CheckboxComp.js
@@ -1,7 +1,7 @@
 import { FormControlLabel, Checkbox } from '@mui/material';
 import { Controller } from 'react-hook-form';
 
-export default function CheckboxComp({ name, control, label, readOnly }) {
+export default function CheckboxComp({ name, control, label, readOnly, onChangeovr }) {
   return (
     <>
       <FormControlLabel
@@ -14,7 +14,12 @@ export default function CheckboxComp({ name, control, label, readOnly }) {
                 {...props}
                 checked={props.value}
                 disabled={readOnly}
-                onChange={(e) => props.onChange(e.target.checked)}
+                onChange={(e) => {
+                  if (onChangeovr !== undefined) {
+                    onChangeovr(e.target.checked);
+                  }
+                  props.onChange(e.target.checked);
+                }}
               />
             )}
           />
