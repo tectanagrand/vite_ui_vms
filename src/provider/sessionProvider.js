79adf46859c12110fd096158b93156addcf4d0ad
tectanagrand <rtektano@gmail.com>
2023-12-18 09:25:13 +0700

    refactor: session provider remove cookies deletion when during mount check

--- src/provider/sessionProvider.js
+++ src/provider/sessionProvider.js
@@ -6,17 +6,35 @@ const SessionContext = createContext();
 
 const SessionProvider = ({ children }) => {
   const [session, setSession_] = useState({
-    fullname: Cookies.get('fullname'),
-    username: Cookies.get('username'),
-    email: Cookies.get('email'),
-    accessToken: Cookies.get('accessToken'),
-    user_id: Cookies.get('user_id'),
-    role: Cookies.get('role'),
-    permission: JSON.parse(sessionStorage.getItem('permission')),
-    groupid: Cookies.get('groupid'),
+    fullname:
+      Cookies.get('fullname') === undefined || Cookies.get('fullname') === 'undefined' ? '' : Cookies.get('fullname'),
+    username:
+      Cookies.get('username') === undefined || Cookies.get('username') === 'undefined' ? '' : Cookies.get('username'),
+    email: Cookies.get('email') === undefined || Cookies.get('email') === 'undefined' ? '' : Cookies.get('email'),
+    accessToken:
+      Cookies.get('accessToken') === undefined || Cookies.get('accessToken') === 'undefined'
+        ? ''
+        : Cookies.get('accessToken'),
+    user_id:
+      Cookies.get('user_id') === undefined || Cookies.get('user_id') === 'undefined' ? '' : Cookies.get('user_id'),
+    role: Cookies.get('role') === undefined || Cookies.get('role') === 'undefined' ? '' : Cookies.get('role'),
+    permission:
+      localStorage.getItem('permission') === undefined || localStorage.getItem('permission') === undefined
+        ? {}
+        : JSON.parse(localStorage.getItem('permission')),
+    groupid:
+      Cookies.get('groupid') === undefined || Cookies.get('groupid') === 'undefined' ? '' : Cookies.get('groupid'),
   });
 
   const setSession = (data) => {
+    Cookies.set('accessToken', data.accessToken);
+    Cookies.set('fullname', data.fullname);
+    Cookies.set('email', data.email);
+    Cookies.set('username', data.username);
+    Cookies.set('user_id', data.user_id);
+    Cookies.set('role', data.role);
+    Cookies.set('groupid', data.groupid);
+    localStorage.setItem('permission', JSON.stringify(data.permission));
     setSession_({
       fullname: data.fullname,
       username: data.username,
@@ -31,52 +49,50 @@ const SessionProvider = ({ children }) => {
   };
 
   const setAccessToken = (act) => {
-    setSession_((prev) => ({ ...prev, accessToken: act }));
+    setSession_({
+      fullname: Cookies.get('fullname'),
+      username: Cookies.get('username'),
+      email: Cookies.get('email'),
+      accessToken: act,
+      user_id: Cookies.get('user_id'),
+      role: Cookies.get('role'),
+      permission: JSON.parse(localStorage.getItem('permission')),
+      groupid: Cookies.get('groupid'),
+    });
   };
   const logOut = () => {
-    setSession_({});
-    sessionStorage.clear();
+    localStorage.clear();
+    Cookies.remove('fullname');
+    Cookies.remove('email');
+    Cookies.remove('username');
+    Cookies.remove('user_id');
+    Cookies.remove('role');
+    Cookies.remove('accessToken');
+    Cookies.remove('groupid');
   };
 
   const getPermission = (page) => {
-    if (sessionStorage.getItem('permission') === null) {
+    if (localStorage.getItem('permission') === null) {
       return '';
     }
-    const permissions = JSON.parse(sessionStorage.getItem('permission'));
+    const permissions = JSON.parse(localStorage.getItem('permission'));
     const curPermission = permissions[page];
     return curPermission;
   };
 
   useEffect(() => {
-    const addSession = setTimeout(() => {
-      if (session.accessToken) {
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + session.accessToken;
-        Cookies.set('accessToken', session.accessToken);
-        Cookies.set('fullname', session.fullname);
-        Cookies.set('email', session.email);
-        Cookies.set('username', session.username);
-        Cookies.set('user_id', session.user_id);
-        Cookies.set('role', session.role);
-        Cookies.set('groupid', session.groupid);
-        sessionStorage.setItem('permission', JSON.stringify(session.permission));
-      } else {
-        Cookies.remove('fullname');
-        Cookies.remove('email');
-        Cookies.remove('username');
-        Cookies.remove('user_id');
-        Cookies.remove('role');
-        Cookies.remove('accessToken');
-        Cookies.remove('groupid');
-        sessionStorage.clear();
-      }
-      return () => {
-        clearTimeout(addSession);
-      };
-    }, 500);
-
-    return () => {
-      clearTimeout(addSession);
-    };
+    // console.log(Cookies.get('accessToken'));
+    if (session.accessToken) {
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + session.accessToken;
+      Cookies.set('accessToken', session.accessToken);
+      Cookies.set('fullname', session.fullname);
+      Cookies.set('email', session.email);
+      Cookies.set('username', session.username);
+      Cookies.set('user_id', session.user_id);
+      Cookies.set('role', session.role);
+      Cookies.set('groupid', session.groupid);
+      localStorage.setItem('permission', JSON.stringify(session.permission));
+    }
   }, [session]);
 
   const contextValue = useMemo(() => ({ session, setSession, logOut, getPermission, setAccessToken }), [session]);
