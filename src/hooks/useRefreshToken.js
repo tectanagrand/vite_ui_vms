ff6279e78e7812467c226a95edaa8096c65a2bd2
tectanagrand <rtektano@gmail.com>
2023-12-18 10:14:15 +0700

    refactor : refresh token expiry auto logout session

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 import { useSession } from 'src/provider/sessionProvider';
 
 const useRefreshToken = () => {
-  const { setAccessToken } = useSession();
+  const { setAccessToken, logOut } = useSession();
   const refresh = async () => {
-    const response = await axios.get(`${process.env.REACT_APP_URL_LOC}/user/refresh`, {
-      withCredentials: true,
-    });
-
-    setAccessToken(response.data.accessToken);
-    return response.data.accessToken;
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_URL_LOC}/user/refresh`, {
+        withCredentials: true,
+      });
+      setAccessToken(response.data.accessToken);
+      return response.data.accessToken;
+    } catch (error) {
+      console.log(error);
+      setTimeout(() => {
+        window.location.replace(`${window.location.hostname}/login`);
+      }, 100);
+    }
   };
 
   return refresh;
