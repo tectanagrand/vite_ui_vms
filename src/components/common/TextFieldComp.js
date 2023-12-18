62453af780f7cb32084536fd8ee0bc583998e322
tectanagrand <rtektano@gmail.com>
2023-12-18 09:33:44 +0700

    refactor: add ref to text field comp

--- src/components/common/TextFieldComp.js
+++ src/components/common/TextFieldComp.js
@@ -9,17 +9,17 @@ export const TextFieldComp = ({ control, label, name, rules, valueovr, readOnly 
         control={control}
         rules={rules}
         defaultValue={valueovr}
-        render={({ field: { onChange, value }, fieldState: { error } }) => (
+        render={({ field: { onChange, value, ref }, fieldState: { error } }) => (
           <TextField
             helperText={error ? error.message : null}
             error={!!error}
             onChange={onChange}
+            inputRef={ref}
             value={value}
             label={label}
             variant="outlined"
             inputProps={{ readOnly: readOnly }}
             fullWidth
-            sx={{ mt: 2, mb: 2 }}
           />
         )}
       />
