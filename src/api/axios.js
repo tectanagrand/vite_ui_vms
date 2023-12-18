21f6f665468da3ba4e2efcefa5654e18286fcf67
tectanagrand <rtektano@gmail.com>
2023-12-18 10:15:45 +0700

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 export const axiosPrivate = axios.create({
-  baseURL: process.env.REACT_APP_URL_LOC,
-  withCredentials: true,
-});
-
-export const axiosPrivWO = axios.create({
   baseURL: process.env.REACT_APP_URL_LOC,
   withCredentials: true,
 });
