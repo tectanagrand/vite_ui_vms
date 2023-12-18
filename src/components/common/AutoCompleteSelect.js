14a4d2650a17f996c82b129489b8b9fa0cbf8459
tectanagrand <rtektano@gmail.com>
2023-12-18 09:29:37 +0700

    refactor: select component to Autocomplete

--- src/components/common/AutoCompleteSelect.js
+++ src/components/common/AutoCompleteSelect.js
@@ -1,25 +1,41 @@
 import { Controller } from 'react-hook-form';
 import { Autocomplete, TextField } from '@mui/material';
 
-export default function AutoCompleteSelect({ name, label, control, options, onChangeovr }) {
+export default function AutoCompleteSelect({
+  name,
+  label,
+  control,
+  options,
+  onChangeovr,
+  freeSolo,
+  readOnly,
+  disable,
+  rules,
+}) {
   return (
     <Controller
       name={name}
       control={control}
-      render={({ field: { onChange, value }, fieldState: { error } }) => {
+      rules={rules}
+      render={({ field: { onChange, value, ref }, fieldState: { error } }) => (
         <Autocomplete
           onChange={(e, newValue) => {
             if (onChangeovr != undefined) {
               onChangeovr(newValue);
             }
-            onChange();
+            onChange(newValue);
           }}
           value={value}
           error={error}
           options={options}
-          renderInput={(params) => <TextField {...params} label={label} />}
-        />;
-      }}
+          freeSolo={freeSolo}
+          autoSelect={freeSolo}
+          fullWidth
+          readOnly={readOnly}
+          disabled={disable}
+          renderInput={(params) => <TextField {...params} label={label} error={error} inputRef={ref} />}
+        />
+      )}
     />
   );
 }
