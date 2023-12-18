0de297341797c9622eab939de398f07d0320c2a8
tectanagrand <rtektano@gmail.com>
2023-12-18 09:27:46 +0700

    add: autocompletebank component

---
+++ src/components/common/AutoCompleteBank.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import useAxiosPrivate from 'src/hooks/useAxiosPrivate';
+import AutoCompleteCustom from './AutoCompleteCustom';
+import ModalCreateBank from './ModalCreateBank';
+import { useEffect, useState } from 'react';
+
+export default function AutoCompleteBank(params) {
+  const country = params.row.country?.value;
+  const axiosPrivate = useAxiosPrivate();
+  const [banksData, setBanksdata] = useState([{ value: '', label: '' }]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [bankName, setBankName] = useState('');
+  const newAddModal = (item) => {
+    setBankName(item);
+    setModalOpen(true);
+  };
+  const handleClose = () => {
+    setModalOpen(false);
+  };
+
+  const modalAccess = (item) => {
+    setModalOpen(item);
+  };
+  useEffect(() => {
+    const getBanks = async () => {
+      try {
+        const bdata = await axiosPrivate.get(`/master/banksap?country=${country}`);
+        const response = bdata.data;
+        const result = response.data;
+        const databank = result?.map((item) => ({
+          value: item.bank_key,
+          label: `${item.bank_key} - ${item.bank_name} ${item.source != null ? '(new)' : ''}`,
+        }));
+        setBanksdata(databank);
+      } catch (error) {
+        console.log(error);
+        alert(error.stack);
+      }
+    };
+    getBanks();
+  }, [country]);
+
+  return (
+    <>
+      <ModalCreateBank
+        openModal={modalOpen}
+        handleClose={handleClose}
+        setModalopen={modalAccess}
+        typepost={'insert'}
+        bankname={bankName}
+        country_code={country}
+        limited={true}
+        params={params}
+      />
+      <AutoCompleteCustom {...params} options={banksData} addnew={true} country={country} newAddModal={newAddModal} />
+    </>
+  );
+}
