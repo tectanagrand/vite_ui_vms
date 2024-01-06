cf941f8ca9757fd53f3fbc2a7f457b4b61b2518a
tectanagrand <rtektano@gmail.com>
2024-01-06 18:49:28 +0700

    feature : edit user info

--- src/components/common/AvatarComp.js
+++ src/components/common/AvatarComp.js
@@ -1,7 +1,7 @@
 import { Avatar, IconButton, Menu, MenuItem, Backdrop, CircularProgress, Box, Typography } from '@mui/material';
 import { useState } from 'react';
 import { useSession } from 'src/provider/sessionProvider';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, redirect } from 'react-router-dom';
 export default function AvatarComp() {
   const { session, logOut } = useSession();
   const [loader, setLoader] = useState(false);
@@ -21,6 +21,10 @@ export default function AvatarComp() {
       navigate('/login');
     }, 1000);
   };
+  const handleUserInfo = () => {
+    setAnchorel(null);
+    navigate(`../../dashboard/account/edit?iduser=${session.user_id}`, { replace: true, state: { page: 'userinfo' } });
+  };
   return (
     <>
       <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
@@ -37,7 +41,20 @@ export default function AvatarComp() {
         onClose={handleClose}
         keepMounted
       >
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        <MenuItem sx={{ width: '10rem' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+            <IconButton onClick={handleMenu}>
+              <Avatar>{session.username?.slice(0, 2).toUpperCase()}</Avatar>
+            </IconButton>
+            <Typography>{session.fullname?.split(' ')[0]}</Typography>
+          </Box>
+        </MenuItem>
+        <MenuItem onClick={handleLogout} sx={{ width: '10rem' }}>
+          Logout
+        </MenuItem>
+        <MenuItem onClick={handleUserInfo} sx={{ width: '10rem' }}>
+          Edit User Info
+        </MenuItem>
       </Menu>
       <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer - 2 }} open={loader}>
         <CircularProgress color="inherit" />
